3dd7f5abaa88cf10865eed1ae5fd190d81442a73
so_1_Hug_Vuog <vuhungtien2000@gmail.com>
2021-08-31 23:01:48 +0700

    fix update product

--- routes/productctl.py
+++ routes/productctl.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 from config.db import conn
 from models.index import productdb,categorydb
 from schemas.index import product
-import numpy as np
+
+
 productctl = APIRouter()
 
 def getCategory(id: int):
@@ -14,7 +15,6 @@ async def showallproduct():
     # sql = "select * from tbl_product,tbl_category where tbl_product.category_id = tbl_category.id"
     # return conn.execute(sql).fetchall()
     rsPd = conn.execute(productdb.select()).fetchall()
-    
     arrRS = []
     for row in rsPd:
         rs = {
@@ -56,9 +56,24 @@ async def findproductbyID(id: int):
 
 @productctl.get("/product/searchname={name}")
 async def findproductbyName(name:str):
-    sql = "select * from tbl_product where `tbl_product`.`product_name` like %s"
-    return conn.execute(sql,"%"+name+"%").fetchall()
-
+    # sql = "select * from tbl_product where `tbl_product`.`product_name` like %s"
+    # return conn.execute(sql,"%"+name+"%").fetchall()
+    rsPd = conn.execute(productdb.select()).fetchall()
+    for row in rsPd:
+        if name in row['product_name']:
+            rs = {
+                "id": row['id'],
+                "product_name": row['product_name'],
+                "category_id": row['category_id'],
+                "product_quantity": row['product_quantity'],
+                "product_price": row['product_price'],
+                "product_image": row['product_image'],
+                "product_description": row['product_description'],
+                "product_hot": row['product_hot'],
+                "category": getCategory(row['category_id'])
+            }
+            break
+    return rs
 
 @productctl.post("/product")
 async def addproduct(newproduct: product):
@@ -79,12 +94,13 @@ async def addproduct(newproduct: product):
 async def updateproduct(id: int, newproduct:product):
     conn.execute(productdb.update().values(
         product_name = newproduct.product_name,
+        category_id = newproduct.category_id,
         product_quantity = newproduct.product_quantity,
         product_price = newproduct.product_price,
         product_image = newproduct.product_image,
         product_description = newproduct.product_description,
         product_hot = newproduct.product_hot
-    ))
+    ).where(productdb.c.id==id))
     return "sua thanh cong"
 
 @productctl.delete("/product/{id}")
