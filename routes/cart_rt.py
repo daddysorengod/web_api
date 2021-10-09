f370ad3c4fc250114bec5975f3bf683cb5c40757
daddysorengod <75688734+daddysorengod@users.noreply.github.com>
2021-10-09 16:44:33 +0700

    fix link cart

--- routes/cart_rt.py
+++ routes/cart_rt.py
@@ -3,6 +3,6 @@ from schemas.index import cart
 from controllers import cart_controller
 cart_rt = APIRouter()
 
-@cart_rt.post("/cart/")
+@cart_rt.post("/cart")
 async def addcart(newcart: cart):
     return cart_controller.addnewCart(newcart)
